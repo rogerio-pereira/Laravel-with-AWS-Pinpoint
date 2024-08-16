93f24b2472bd13639d994b4cb7bb5a4213099e32
Rogerio Pereira <hi@rogeriopereira.dev>
2024-08-16 14:50:51 -0400

    Add a Log in Purchase Listener

--- app/Listeners/PurchaseListener.php
+++ app/Listeners/PurchaseListener.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\PurchaseEvent;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
 
 class PurchaseListener
 {
@@ -21,6 +22,6 @@ class PurchaseListener
      */
     public function handle(PurchaseEvent $event): void
     {
-        //
+        Log::info('Event Purchase', $event->purchase->toArray());
     }
 }
