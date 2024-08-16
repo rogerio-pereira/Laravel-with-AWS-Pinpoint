699ee15b7453418d5541cbf74b329b960058774b
Rogerio Pereira <hi@rogeriopereira.dev>
2024-08-16 11:40:16 -0400

    Create Produc model, factory, migration, seeder... Calls productSeeder in Database seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Products>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->string(),
+            'price' => rand(50,100),
+        ];
+    }
+}
